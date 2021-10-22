be18c0491cc74cd38b32d9d3b24667551dff7444
Allovaro <allovaro@gmail.com>
2021-10-22 16:57:05 +0300

    react template added

---
+++ client/src/components/app/app.js
@@ -0,0 +1,11 @@
+import './app.css';
+
+function App() {
+    return (
+        <div className="App">
+            Hello ToDo List
+        </div>
+    );
+}
+
+export default App;
--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -4,21 +4,17 @@ pragma solidity >=0.5.0 <0.9.0;
 contract TodoList {
     address public owner;
     struct Task {
-        uint id;
-        uint data;
+        uint date;
         string content;
         bool done;
     }
-    uint lastTaskId;
-    uint[] taskIds;
-    mapping(uint => Task) tasks;
+    Task[] tasks;
 
     constructor(address _owner) {
         owner = _owner;
-        lastTaskId = 0;
     }
 
-    event TaskCreated(uint _id, uint _date, string _content, bool _done);
+    event TaskCreated(uint _date, string _content, bool _done);
 
     modifier onlyOwner {
         require(msg.sender  == owner, "Only owner can call this function");
@@ -26,15 +22,18 @@ contract TodoList {
     }
 
     modifier taskExist(uint id) {
-        require(tasks[id].id != 0, "Task doesn't exist");
+        require(id < tasks.length, "Task doesn't exist");
         _;
     }
 
     function createTask(string memory _task) public onlyOwner {
-        lastTaskId += 1;
-        tasks[lastTaskId] = Task(lastTaskId, block.timestamp, _task, false);
-        taskIds.push(lastTaskId);
-        emit TaskCreated(lastTaskId, block.timestamp, _task, false);
+        tasks.push(Task(block.timestamp, _task, false));
+        emit TaskCreated(block.timestamp, _task, false);
+    }
+    
+    function deleteTask(uint _id) public onlyOwner {
+        tasks[_id] = tasks[tasks.length - 1];
+        tasks.pop();
     }
 
     function doneUndoneTask(uint _id) public onlyOwner taskExist(_id) {
@@ -42,23 +41,17 @@ contract TodoList {
     }
 
     function getLastTaskId() public view returns(uint) {
-        return lastTaskId;
-    }
-
-    function getTaskIds() public view returns(uint[] memory) {
-        return taskIds;
+        return tasks.length - 1;
     }
 
     function getTask(uint _id) public view onlyOwner taskExist(_id)
     returns(
         uint,
-        uint,
         string memory,
         bool
     ) {
         return(
-            _id,
-            tasks[_id].data,
+            tasks[_id].date,
             tasks[_id].content,
             tasks[_id].done
         );
